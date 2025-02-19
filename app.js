17aa7a1a9518f9eb1091c80a5cfbaccd1138006e
Luis <losjuegosdelcapibara@gmail.com>
2025-02-19 17:40:55 -0600

    Se agrego funcion que randomiza numero entre 1 -10

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-
+let numerosecreto = generarNumeroSecreto();
+console.log(numerosecreto);
 
 function asignartextoElemento(elemento, texto){
     let elementoHtml = document.querySelector(elemento);
@@ -8,7 +9,16 @@ function asignartextoElemento(elemento, texto){
 
 function intentoDeUsuario(){
     alert("Olis");
+    return;
+}
+
+
+function generarNumeroSecreto() {
+    return Math.floor(Math.random()*10 + 1);
+
+    
 }
 
 asignartextoElemento('h1',"EL NUMERO SECRETO");
 asignartextoElemento('p','Indica un numero del 1 al 10!' );
+
